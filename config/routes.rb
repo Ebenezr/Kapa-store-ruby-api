450a5951f03fda41c5f4184923519e037d06fad9
Ebenezr <ebenezarbukosia@gmail.com>
2023-02-10 11:02:09 +0300

    [ADD] cors and env setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  resources :addresses
+  resources :reviews
+  resources :orders
+  resources :user_payments
+  resources :order_items
+  resources :discounts
+  resources :inventories
+  resources :products
+  resources :categories
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
